7f03a869cfcc6e223c0f15f96171d294b03a5b60
Desire Kabwanga <dkay1101@gmail.com>
2021-10-04 00:45:54 +0200

    user can now enter in any room from index page

--- static/index.js
+++ static/index.js
@@ -1,46 +1,23 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const chatRoomName = document.querySelector('#room-name-input')
-//     document.querySelector('#room-name-input').focus();
-//     document.querySelector('#room-name-input').onkeyup = function (e) {
-//         if (e.keyCode === 13) {  // enter, return
-//             document.querySelector('#room-name-submit').click();
-//             document.querySelector('#room-name-input').value('')
-//             chatRoomName = ''
-//         }
-//     };
 
-//     document.querySelector('#room-name-submit').onclick = function (e) {
-//         let roomName = document.querySelector('#room-name-input').value
-//         renderButton(roomName)
-//     };
-// });
- 
-// const renderButton = (roomName) => {
-//     const newButton = document.createElement("button")
-//     newButton.textContent = roomName
-//     newButton.type = "button"
-//     newButton.classList = "btn  btn-warning mb-4 w-60"
-
-//     newButton.onclick = () => {
-//         window.location.pathname = '/chat/' + roomName + '/'
-//     }
-
-//     document.getElementById("pnlRenderedButton").appendChild(newButton)
-// }
 
 document.querySelector('#room-name-input').focus();
-document.querySelector('#room-name-input').onkeyup = function(e) {
+document.querySelector('#room-name-input').onkeyup = function (e) {
     if (e.keyCode === 13) {  // enter, return
         document.querySelector('#room-name-submit').click();
     }
 };
 
-document.querySelector('#room-name-submit').onclick = function(e) {
+document.querySelector('#room-name-submit').onclick = function (e) {
     var roomName = document.querySelector('#room-name-input').value;
     window.location.pathname = '/chat/' + roomName + '/';
 };
 
-document.querySelector('.myRoom').onclick = (e) => {
-    const roomName = e.target.id
-    window.location.pathname = '/chat/' + roomName + '/'
+
+const myRoomListener = (item) => {
+    item.onclick = (e) => {
+        const roomName = e.target.id
+        window.location.pathname = '/chat/' + roomName + '/'
+    }
 }
+
+document.querySelectorAll('.myRoom').forEach(myRoomListener)
--- static/room.js
+++ static/room.js
@@ -1,4 +1,4 @@
-const chatLog =document.querySelector('#chat-log')
+const chatLog = document.querySelector('#chat-log')
 // get room name
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
 
@@ -13,45 +13,55 @@ const chatSocket = new WebSocket(
 );
 
 // runs when we recieve a msg from the websocket. take data from website and display in chat log div
-chatSocket.onmessage = function(e) {
+chatSocket.onmessage = function (e) {
     const data = JSON.parse(e.data);
     const messageElement = document.createElement('div')
     const userId = data['user_id']
     const loggedInUserId = JSON.parse(document.getElementById('user_id').textContent)
     // console.log(userId)
     messageElement.innerText = data.message
-    
+
     if (userId === loggedInUserId) {
         messageElement.classList.add('message', 'sender')
-    } 
+    }
     else {
         messageElement.classList.add('message', 'receiver')
     }
-    
+
 
     chatLog.appendChild(messageElement)
 
-    if(document.querySelector('#emptyText')){
+    if (document.querySelector('#emptyText')) {
         document.querySelector('#emptyText').remove()
     }
+    document.querySelector('#chat-log').scrollTo(0, document.querySelector('#chat-log').scrollHeight)
 };
 
-chatSocket.onclose = function(e) {
+chatSocket.onclose = function (e) {
     console.error('Chat socket closed unexpectedly');
 };
 
 document.querySelector('#chat-message-input').focus();
-document.querySelector('#chat-message-input').onkeyup = function(e) {
+document.querySelector('#chat-message-input').onkeyup = function (e) {
     if (e.keyCode === 13) {  // enter, return
         document.querySelector('#chat-message-submit').click();
     }
 };
 
-document.querySelector('#chat-message-submit').onclick = function(e) {
+document.querySelector('#chat-message-submit').onclick = function (e) {
     const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
-    chatSocket.send(JSON.stringify({
-        'message': message
-    }));
+    if (message) {
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+
+        document.querySelector('#chat-log').scrollTo(0, document.querySelector('#chat-log').scrollHeight)
+
+    }
+
     messageInputDom.value = '';
+
+
+
 };
